7c1d08c191271184021fdf1618cb25e231f87d60
Tatyana563 <nikolaev.english@gmail.com>
2020-01-20 13:34:31 +0200

    Init

--- src/dao/CountryDAO.java
+++ src/dao/CountryDAO.java
@@ -19,7 +19,7 @@ public class CountryDAO {
             Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery("select * from country");
             while (rs.next()) {
-                int id = rs.getInt("id");
+                int id = rs.getInt("country_id");
                 String name = rs.getString("name");
                 result.add(new Country(id,name));
             }
@@ -30,8 +30,27 @@ public class CountryDAO {
     }
 
     public void add(String countryName) {
-        try (PreparedStatement preparedStatement = connection.prepareStatement("insert into country (name) values (?)")) {
+        try (PreparedStatement preparedStatement =
+                     connection.prepareStatement("insert into country (name) values (?)")) {
             preparedStatement.setString(1, countryName);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void update(String name, int id){
+        try (PreparedStatement preparedStatement = connection.prepareStatement("UPDATE country SET name =? " +
+                "WHERE country_id = ?")) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setInt(2, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void delete(int id){
+        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM country AS c WHERE c.country_id = ?")) {
+            preparedStatement.setInt(1, id);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
--- src/server/DataBaseServlet.java
+++ src/server/DataBaseServlet.java
@@ -1,6 +1,7 @@
 package server;
 
 import beans.CountryBean;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dao.CountryDAO;
 import tables.Country;
 
@@ -11,44 +12,94 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.List;
-
+import java.util.Objects;
+//http://localhost:9999/
 @WebServlet(name = "DataBaseServlet", urlPatterns = {"/countries"})
 public class DataBaseServlet extends HttpServlet {
+    public static final String URL = "jdbc:postgresql://localhost:5432/example?" +
+            "user=postgres&password=apple25";
 
-    @Resource(name = "jdbc/example")
-    DataSource ds;
+ /*   @Resource(name = "jdbc/example")
+    DataSource ds;*/
 
     private CountryDAO countryDAO;
+    private Connection connection;
+
+    private Connection getConnection() {
+        if (Objects.isNull(connection)) {
+            try {
+                connection = DriverManager.getConnection(URL);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
 
     @Override
     public void init() throws ServletException {
         super.init();
         try {
-            Connection connection = ds.getConnection();
-            try(Statement statement = connection.createStatement()) {
-                statement.execute("set search_path to demo");
+            Class.forName("org.postgresql.Driver");
+            countryDAO = new CountryDAO(getConnection());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*@Override
+    public void init() throws ServletException {
+        super.init();
+        try {
+            Connection connection = getConnection();
+            try (Statement statement = connection.createStatement()) {
+//                statement.execute("set search_path to demo");
             }
             countryDAO = new CountryDAO(connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-
+    }*/
+    //http://localhost:9999/countries?country_name=Italy
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String countryName = request.getParameter("country_name");
         countryDAO.add(countryName);
-        response.sendRedirect("countries");
+        response.sendRedirect("countries.jsp");
     }
-
+//http://localhost:9999/countries
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         CountryBean countryBean = new CountryBean();
         countryBean.setCountries(countryDAO.findAll());
         request.setAttribute("countryBean", countryBean);
-        request.getRequestDispatcher("/countries.jsp").forward(request,response);
+        request.getRequestDispatcher("/countries.jsp").forward(request, response);
+    }
+
+    //http://localhost:9999/countries?country=Italy&id=1
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Country country = null;
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            BufferedReader reader = req.getReader();
+            country = mapper.readValue(reader, Country.class);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String countryName = (req.getParameter("country"));
+        int countryId = Integer.parseInt(req.getParameter("id"));
+        countryDAO.update(countryName, countryId);
+    }
+//http://localhost:9999/countries?countryId=3
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("countryId"));
+        countryDAO.delete(id);
     }
 }
